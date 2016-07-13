406fb2586024793bb9da8535912b0562fda873fe
David Tran <eternallite@users.noreply.github.com>
2016-07-13 15:51:39 -0400

    RGBA format for effect composer

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = function(THREE) {
     if ( renderTarget === undefined ) {
       var width = window.innerWidth || 1;
       var height = window.innerHeight || 1;
-      var parameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat, stencilBuffer: false };
+      var parameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat, stencilBuffer: false };
 
       renderTarget = new THREE.WebGLRenderTarget( width, height, parameters );
     }
